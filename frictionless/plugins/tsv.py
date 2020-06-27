46608c4a0a05f570affe041b8c3ffa280cc30d94
roll <eskarev@gmail.com>
2020-06-27 09:57:19 +0300

    Migrated tsv

--- frictionless/plugins/tsv.py
+++ frictionless/plugins/tsv.py
@@ -1,60 +1,48 @@
 import tsv
 from ..parser import Parser
 from ..plugin import Plugin
-from .. import helpers
+from ..dialects import Dialect
 
 
 # Plugin
 
 
 class TsvPlugin(Plugin):
-    def create_parser(self, source, *, control=None, dialect=None):
-        pass
+    def create_parser(self, file):
+        if file.format == 'tsv':
+            return TsvParser(file)
 
 
-# Parsers
+# Parser
 
 
 class TsvParser(Parser):
-    options = []  # type: ignore
+    Dialect = property(lambda self: TsvDialect)
 
-    def __init__(self, loader):
-        self.__loader = loader
-        self.__extended_rows = None
-        self.__encoding = None
-        self.__chars = None
+    # Read
 
-    @property
-    def closed(self):
-        return self.__chars is None or self.__chars.closed
-
-    def open(self, source, encoding=None):
-        self.close()
-        self.__chars = self.__loader.load(source, encoding=encoding)
-        self.__encoding = getattr(self.__chars, 'encoding', encoding)
-        if self.__encoding:
-            self.__encoding.lower()
-        self.reset()
-
-    def close(self):
-        if not self.closed:
-            self.__chars.close()
-
-    def reset(self):
-        helpers.reset_stream(self.__chars)
-        self.__extended_rows = self.__iter_extended_rows()
-
-    @property
-    def encoding(self):
-        return self.__encoding
-
-    @property
-    def extended_rows(self):
-        return self.__extended_rows
-
-    # Private
-
-    def __iter_extended_rows(self):
-        items = tsv.un(self.__chars)
+    def read_cell_stream_create(self):
+        items = tsv.un(self.loader.text_stream)
         for row_number, item in enumerate(items, start=1):
             yield (row_number, None, list(item))
+
+
+# Dialect
+
+
+class TsvDialect(Dialect):
+    """Tsv dialect representation
+
+    # Arguments
+        descriptor? (str|dict): descriptor
+
+    # Raises
+        FrictionlessException: raise any error that occurs during the process
+
+    """
+
+    metadata_profile = {  # type: ignore
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {},
+    }
