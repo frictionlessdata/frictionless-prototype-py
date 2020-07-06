cb0504b358d1e69379f1d5422d7a289097293428
roll <eskarev@gmail.com>
2020-07-06 11:03:09 +0300

    Added resource/package profiles

--- frictionless/helpers.py
+++ frictionless/helpers.py
@@ -60,6 +60,20 @@ def ensure_dir(path):
         os.makedirs(dirpath)
 
 
+def is_safe_path(path):
+    contains_windows_var = lambda val: re.match(r"%.+%", val)
+    contains_posix_var = lambda val: re.match(r"\$.+", val)
+    unsafeness_conditions = [
+        os.path.isabs(path),
+        ("..%s" % os.path.sep) in path,
+        path.startswith("~"),
+        os.path.expandvars(path) != path,
+        contains_windows_var(path),
+        contains_posix_var(path),
+    ]
+    return not any(unsafeness_conditions)
+
+
 def parse_hashing_algorithm(hash):
     if not hash:
         return "md5"
---
+++ frictionless/package.py
@@ -0,0 +1,5 @@
+from .metadata import Metadata
+
+
+class Resource(Metadata):
+    pass
