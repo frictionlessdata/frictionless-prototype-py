22251f78c2d1370c8aee1b185b90aadfcad6dc93
roll <eskarev@gmail.com>
2020-07-15 09:54:32 +0300

    Implemented describe

--- frictionless/describe/main.py
+++ frictionless/describe/main.py
@@ -1,41 +1,18 @@
-from ..schema import Schema
-from ..table import Table
-from .. import config
+import glob
+from importlib import import_module
 
 
-# TODO: support source_type (table/resource/package)
-# TODO: support detect_type
-def describe(
-    source,
-    *,
-    source_type=None,
-    headers=None,
-    expand=False,
-    missing_values=None,
-    infer_type=None,
-    infer_names=None,
-    infer_volume=config.DEFAULT_INFER_VOLUME,
-    infer_confidence=config.DEFAULT_INFER_CONFIDENCE
-):
-    """Describe data source
+def describe(source, *, source_type=None, **options):
+    module = import_module("frictionless.describe")
 
-    # Arguments
-        source
-        expand
-        missing_values
-        infer_type
-        infer_names
-        infer_volume
-        infer_confidence
+    # Detect source type
+    if not source_type:
+        source_type = "resource"
+        if isinstance(source, list):
+            source_type = "package"
+        elif glob.has_magic(source):
+            source_type = "package"
 
-    """
-    with Table(source, headers=headers, infer_volume=infer_volume) as table:
-        schema = Schema.from_sample(
-            table.sample,
-            names=infer_names or table.headers,
-            confidence=infer_confidence,
-            missing_values=missing_values,
-        )
-        if expand:
-            schema.expand()
-        return schema
+    # Describe source
+    describe = getattr(module, "describe_%s" % source_type)
+    return describe(source, **options)
