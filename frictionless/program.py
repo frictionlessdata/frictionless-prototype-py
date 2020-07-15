28d56b49afd1a4d78bccc59c293cca5b73a78368
roll <eskarev@gmail.com>
2020-07-15 18:28:39 +0300

    Added DEVT cli commands

--- frictionless/program.py
+++ frictionless/program.py
@@ -2,13 +2,61 @@ import click
 from sys import exit
 import json as json_module
 from pprint import pformat
+from .describe import describe
+from .extract import extract
 from .validate import validate
+from .transform import transform
 from . import config
 
 
 # General
-@click.command(name="frictionless", short_help="Validate tabular files")
+
+
+@click.group(name="frictionless")
 @click.version_option(config.VERSION, message="%(version)s", help="Print version")
+def program():
+    pass
+
+
+# Describe
+
+
+@program.command(name="describe")
+@click.argument("source", type=click.Path(), nargs=-1, required=True)
+@click.option("--source-type", type=str, help="Source type")
+@click.option("--json", is_flag=True, help="Output report as JSON")
+# File
+@click.option("--scheme", type=str, help="File scheme")
+@click.option("--format", type=str, help="File format")
+@click.option("--hashing", type=str, help="File hashing")
+@click.option("--encoding", type=str, help="File encoding")
+@click.option("--compression", type=str, help="File compression")
+@click.option("--compression-path", type=str, help="File compression path")
+# Schema
+@click.option("--sync-schema", is_flag=True, help="Sync schema")
+@click.option("--infer-type", type=str, help="Infer type")
+@click.option("--infer-names", type=str, multiple=True, help="Infer names")
+@click.option("--infer-sample", type=int, help="Infer sample")
+@click.option("--infer-confidence", type=float, help="Infer confidence")
+@click.option("--infer-missing-values", type=str, multiple=True, help="Infer missing")
+def program_describe(source, *, source_type, json, **options):
+    for key, value in list(options.items()):
+        if not value:
+            del options[key]
+        elif isinstance(value, tuple):
+            options[key] = list(value)
+    source = list(source) if len(source) > 1 else source[0]
+    result = describe(source, source_type=source_type, **options)
+    if json:
+        return click.secho(json_module.dumps(result, indent=2, ensure_ascii=False))
+    click.secho(pformat(result.to_dict()))
+
+
+# Extract
+
+
+# TODO: use tabulate?
+@program.command(name="extract")
 @click.argument("source", type=click.Path(), required=True)
 @click.option("--source-type", type=str, help="Source type")
 @click.option("--json", is_flag=True, help="Output report as JSON")
@@ -19,16 +67,41 @@ from . import config
 @click.option("--encoding", type=str, help="File encoding")
 @click.option("--compression", type=str, help="File compression")
 @click.option("--compression-path", type=str, help="File compression path")
-# Table
+# Schema
+@click.option("--sync-schema", is_flag=True, help="Sync schema")
+@click.option("--infer-type", type=str, help="Infer type")
+@click.option("--infer-names", type=str, multiple=True, help="Infer names")
+@click.option("--infer-sample", type=int, help="Infer sample")
+@click.option("--infer-confidence", type=float, help="Infer confidence")
+@click.option("--infer-missing-values", type=str, multiple=True, help="Infer missing")
+def program_extract(source, *, source_type, json, **options):
+    for key, value in list(options.items()):
+        if not value:
+            del options[key]
+        elif isinstance(value, tuple):
+            options[key] = list(value)
+    result = extract(source, source_type=source_type, **options)
+    if json:
+        return click.secho(json_module.dumps(result, indent=2, ensure_ascii=False))
+    click.secho(pformat(result))
+
+
+# Validate
+
+
+@program.command(name="validate")
+@click.argument("source", type=click.Path(), required=True)
+@click.option("--source-type", type=str, help="Source type")
+@click.option("--json", is_flag=True, help="Output report as JSON")
+# Headers
 @click.option("--headers", type=int, multiple=True, help="Headers")
-@click.option("--pick-fields", type=str, multiple=True, help="Pick fields")
-@click.option("--skip-fields", type=str, multiple=True, help="Skip fields")
-@click.option("--limit-fields", type=int, help="Limit fields")
-@click.option("--offset-fields", type=int, help="Offset fields")
-@click.option("--pick-rows", type=str, multiple=True, help="Pick rows")
-@click.option("--skip-rows", type=str, multiple=True, help="Skip rows")
-@click.option("--limit-rows", type=int, help="Limit rows")
-@click.option("--offset-rows", type=int, help="Offset rows")
+# File
+@click.option("--scheme", type=str, help="File scheme")
+@click.option("--format", type=str, help="File format")
+@click.option("--hashing", type=str, help="File hashing")
+@click.option("--encoding", type=str, help="File encoding")
+@click.option("--compression", type=str, help="File compression")
+@click.option("--compression-path", type=str, help="File compression path")
 # Schema
 @click.option("--schema", type=click.Path(), help="Schema")
 @click.option("--sync-schema", is_flag=True, help="Sync schema")
@@ -36,17 +109,18 @@ from . import config
 @click.option("--infer-names", type=str, multiple=True, help="Infer names")
 @click.option("--infer-sample", type=int, help="Infer sample")
 @click.option("--infer-confidence", type=float, help="Infer confidence")
-# Integrity
-@click.option("--stats-hash", type=str, help="Expected hash based on hashing option")
-@click.option("--stats-bytes", type=int, help="Expected size in bytes")
+@click.option("--infer-missing-values", type=str, multiple=True, help="Infer missing")
 # Validation
+@click.option("--checksum-hash", type=str, help="Expected hash based on hashing option")
+@click.option("--checksum-bytes", type=int, help="Expected size in bytes")
+@click.option("--checksum-rows", type=int, help="Expected size in bytes")
 @click.option("--pick-errors", type=str, multiple=True, help="Pick errors")
 @click.option("--skip-errors", type=str, multiple=True, help="Skip errors")
 @click.option("--limit-errors", type=int, help="Limit errors")
 @click.option("--limit-memory", type=int, help="Limit memory")
 # Package/Resource
 @click.option("--noinfer", type=bool, help="Validate metadata as it is")
-def program(source, *, source_type, json, **options):
+def program_validate(source, *, headers, source_type, json, **options):
     for key, value in list(options.items()):
         if not value:
             del options[key]
@@ -57,13 +131,28 @@ def program(source, *, source_type, json, **options):
     exit(int(not report["valid"]))
 
 
+# Transform
+
+
+@program.command(name="transform")
+@click.argument("source", type=click.Path(), required=True)
+def program_transform(source):
+    transform(source)
+
+
+# Server
+# TODO: implement
+
+
 # Internal
 
 
+# TODO: rewrite this function
 def print_report(report, output=None, json=False):
     if json:
-        return click.secho(json_module.dumps(report, indent=2))
+        return click.secho(json_module.dumps(report, indent=2, ensure_ascii=False))
     color = "green" if report.valid else "red"
+    report = report.to_dict()
     tables = report.pop("tables")
     errors = report.pop("errors")
     click.secho("REPORT", bold=True)
