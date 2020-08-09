c697cceabc377425b42932ff0836935639580aa7
roll <eskarev@gmail.com>
2020-08-09 12:02:13 +0300

    Updated readme

--- docs/migration-guide.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-
-source = "MIGRATION.md"
-target_dir = os.path.join("docs", "build", "migration-guide")
-target = os.path.join(target_dir, "README.md")
-os.makedirs(target_dir, exist_ok=True)
-shutil.copy(source, target)
