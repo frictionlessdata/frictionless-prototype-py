b7dbb66cf5b17dd32ad160d6c8ceb8f835c02dab
roll <eskarev@gmail.com>
2020-08-19 10:03:35 +0300

    Added StorageTable class

--- frictionless/storage.py
+++ frictionless/storage.py
@@ -1,20 +1,37 @@
 class Storage:
-    @property
-    def resources(self):
+    def __init__(self, **options):
         pass
 
     @property
-    def resource_names(self):
+    def tables(self):
         pass
 
-    def add_resource(self, *descriptors, **options):
+    @property
+    def table_names(self):
         pass
 
-    def get_resource(self, name):
+    def add_table(self, *tables, **options):
         pass
 
-    def has_resource(self, name):
+    def get_table(self, name):
         pass
 
-    def remove_resource(self, *names, **options):
+    def has_table(self, name):
         pass
+
+    def remove_table(self, *names, **options):
+        pass
+
+
+class StorageTable:
+    def __init__(self, name, *, schema):
+        self.__name = name
+        self.__schema = schema
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def schema(self):
+        return self.__schema
