c5cc4faa7c0bc9c3d005ef6b77c6f44c6ab8e8d4
roll <eskarev@gmail.com>
2020-09-12 17:01:43 +0300

    Added basepath and trusted to CLI

--- frictionless/program/validate.py
+++ frictionless/program/validate.py
@@ -36,6 +36,8 @@ from .main import program
 @click.option("--limit-errors", type=int, help="Limit errors")
 @click.option("--limit-memory", type=int, help="Limit memory")
 # Package/Resource
+@click.option("--basepath", type=str, help="Package basepath")
+@click.option("--trusted", is_flag=True, help="Allow unsafe paths")
 @click.option("--noinfer", type=bool, help="Validate metadata as it is")
 def program_validate(source, *, source_type, json, **options):
     """Validate data
